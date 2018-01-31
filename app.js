3e57cf07f56eb40b007df91076f25657986edc8f
Rodrigo Siviero <rodrigo.siviero@concrete.com.br>
2018-01-31 07:25:37 -0200

    dockerfile

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var debug = require('debug');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +28,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/sky', sky);
+
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+  //console.log('listening' + server.address().port)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
